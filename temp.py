1e206ef77cbe6e489cd49f7a90040b4def0c5abf
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-06-17 18:04:52 +0900

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+# API_key = "71a7fd6193ea09d86d2d1cc3c79bb259"
+url = "https://api.openweathermap.org/data/2.5/weather?q=Sinchang-myeon&units=metric&appid=71a7fd6193ea09d86d2d1cc3c79bb259"
+
+
+res = requests.get(url)
+wethaerRes = json.loads(res.text)
+def getWeather():
+    print(wethaerRes['weather'][0]['id'])
+    print(wethaerRes['main']['temp'])
+    weatherId = wethaerRes['weather'][0]['id']
+    temp = wethaerRes['main']['temp']
+    return temp, weatherId
+
+t,w = getWeather()
+# getWeather()
