dabf5e3dc18cfad0eeb108d6e3b5dbbff44abbae
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-06-21 10:59:31 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # API_key = "71a7fd6193ea09d86d2d1cc3c79bb259"
-url = "https://api.openweathermap.org/data/2.5/weather?q=Sinchang-myeon&units=metric&appid=71a7fd6193ea09d86d2d1cc3c79bb259"
+url = "https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&appid=71a7fd6193ea09d86d2d1cc3c79bb259"
 
 
 res = requests.get(url)
